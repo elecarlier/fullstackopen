f0c1d1a7b76518b2f7b966d7521f6024305e7810
elecarlier <eleonorecarlier@hotmail.be>
2025-10-04 12:57:42 +0200

    step 3 and 4

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,6 +2,24 @@ import { useState } from 'react'
 
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
+
+const History = (props) => {  
+  if (props.allClicks == 0) {
+    return (     
+      <div>        
+          No Feedback given    
+      </div>    
+    )  
+  }  
+  return (    
+    <div>      
+      <Disp name='good' nb={props.good}/>
+      <Disp name='neutral' nb={props.neutral}/>
+      <Disp name='bad' nb={props.bad}/>
+      <Stat good={props.good} neutral={props.neutral} bad={props.bad} />
+    </div>  )
+    }
+
 const Disp = (props) =>
 {
   return (
@@ -29,20 +47,24 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [allClicks, setAll] = useState(0)
 
   const handleGood = () =>
   {
       setGood(good + 1)
+      setAll(allClicks + 1)
   }
 
   const handleNeutral= () =>
   {
     setNeutral(neutral + 1)
+    setAll(allClicks + 1)
   } 
 
   const handleBad = () =>
   {
       setBad(bad +1)
+      setAll(allClicks + 1)
   }
 
   return (
@@ -59,10 +81,7 @@ const App = () => {
       <h1>
       Statistics
       </h1>
-      <Disp name='good' nb={good}/>
-      <Disp name='neutral' nb={neutral}/>
-      <Disp name='bad' nb={bad}/>
-      <Stat good={good} neutral={neutral} bad={bad} />
+      <History allClicks={allClicks} good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
